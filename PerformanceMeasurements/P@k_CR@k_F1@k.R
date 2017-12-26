00b8a046a5f80e91c5e112f4457b8c905f5d142f
JuliaHilber <JuliaHilber@users.noreply.github.com>
2017-12-26 15:37:35 +0100

    Add files via upload

--- PerformanceMeasurements/P@k_CR@k_F1@k.R
+++ PerformanceMeasurements/P@k_CR@k_F1@k.R
@@ -5,44 +5,66 @@ data <- xmlParse("/Volumes/My Passport for Mac/div-2014/devset/devset_topics.xml
 xml_data <- xmlToList(data)
 
 
-# load the file with relevance scores
-result <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/Measurements/relevance.txt")
+# load the result file
+result <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/result.txt")
 k <- 50
 
 # some more variables
-locations <- 30
+locations <- length(xml_data)
 allphotos <- k*locations
+# relevant and retrieved -> rar
 rar <- 0
+n <- 0
+nc <- 0
 
 # go over all locations
 for(i in 1:length(xml_data)) {
   # get some parameter
-  data <- xml_data[i]
-  location <- data$topic$title
-  number <- data$topic$number
+  data1 <- xml_data[i]
+  location <- data1$topic$title
+  number <- data1$topic$number
   
   # load the relevant ground truth of that location
   gt <- read.table(paste0("/Volumes/My Passport for Mac/div-2014/devset/gt/rGT/", location, " rGT.txt"), sep=",")
+  gt1 <- read.table(paste0("/Volumes/My Passport for Mac/div-2014/devset/gt/dGT/", location, " dclusterGT.txt"), sep=",", quote="")
+  gt2 <- read.table(paste0("/Volumes/My Passport for Mac/div-2014/devset/gt/dGT/", location, " dGT.txt"), sep=",")
+    
+  n <- n + nrow(gt1)
  
-  # get all photo id's of the relevance score file
-  rel <- result[which(result[, 1] == location), ]
+  # get all photo id's of the result file
+  rel <- result[which(result[, 1] == number), ]
   rel <- rel[1:k, ]
   
-  # if(the picture is relevant (get this from the ground truth)) then add 1 to the variable rar (relevant and retrieved)
-  for(i in 1:k) {
-    id <- rel[i, 2]
+  list <- c(0)
+  for(j in 1:k) {
+    id <- rel[j, 3]
+    # if(the picture is relevant (get this from the ground truth)) then add 1 to the variable rar (relevant and retrieved)
     if(gt[which(gt[, 1] == id), 2] == 1) {
       rar <- rar + 1
     }
+    # if(the cluster with the picture is in the list) then do nothing, else add it to the list
+    if(id %in% gt2[, 1]) {
+      clusterno <- gt2[which(gt2[, 1] == id), 2]
+      if(clusterno %in% list) {
+        #do nothing
+      } else {
+        list <- append(list, clusterno)
+      }
+    }
   }
+  nc <- nc + length(list)
 }
 
 # calculate precision@k (P@k)
 precision <- rar/allphotos
 
 # calculate cluster recall @ k (CR@k)
-recall <- 1
+recall <- nc/n
 
-# calculate f1@k
+# calculate F1@k
 f1 <- 2*(precision*recall)/(precision+recall)
-  
+
+# print the performance measurements
+precision
+recall
+f1
