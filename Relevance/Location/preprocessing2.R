e66c04bd3fdef519aa0d8032d4ccb16d5deff407
JuliaHilber <JuliaHilber@users.noreply.github.com>
2017-12-29 18:26:44 +0100

    Add files via upload

---
+++ Relevance/Location/preprocessing2.R
@@ -0,0 +1,16 @@
+location <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/diversity/location_filter_result.txt")
+tfidf <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/MSR/Relevance/TFIDF/similarityScores.txt")
+
+# get only the half of the file
+tfidf <- tfidf[1:(nrow(tfidf)/2), ]
+
+result <- data.frame()
+
+# calculate new similarity score with (tfidf+location)/2
+for(i in 1:nrow(tfidf)) {
+  id <- tfidf[i, 2]
+  sim <- tfidf[i, 3] + location[which(location[, 2] == id), 3]/2
+  result <- rbind(result, c(tfidf[i, 1], id, sim))
+}
+
+write.table(result, file="tfidf_location_SimilarityScores.txt", row.names = FALSE, col.names = FALSE)
