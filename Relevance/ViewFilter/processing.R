96eb05f74327900e486b850b03fd47e7b8fd96e7
JuliaHilber <JuliaHilber@users.noreply.github.com>
2018-01-04 13:52:43 +0000

    Add files via upload

---
+++ Relevance/ViewFilter/processing.R
@@ -0,0 +1,13 @@
+postprocessing <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/MSR/Relevance/Postprocessing/data/relevancyScores.txt")
+views <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/Relevance/views.txt")
+
+result <- data.frame()
+
+# calculate new similarity score with 0.5*postprocessing + 0.5*views
+for(i in 1:nrow(postprocessing)) {
+  id <- postprocessing[i, 2]
+  sim <- 0.75*postprocessing[i, 3] + 0.25*views[which(views[, 2] == id), 3]
+  result <- rbind(result, c(postprocessing[i, 1], id, sim))
+}
+
+write.table(result, file="/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/Relevance/SimilarityScores.txt", row.names = FALSE, col.names = FALSE)
