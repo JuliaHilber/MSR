68a4fc40bafe219343a6d5a1461d9aaeb996c418
JuliaHilber <JuliaHilber@users.noreply.github.com>
2018-01-11 20:36:59 +0100

    Add files via upload

--- Relevance/Location/Preprocessing/preprocessing2.R
+++ Relevance/Location/Preprocessing/preprocessing2.R
@@ -1,16 +1,29 @@
-location <- read.table("C:/Users/Alina/Documents/University/Multimedia Search & Retrieval/project/MSR/resultfiles/location_filter_result.txt")
-tfidf <- read.table("C:/Users/Alina/Documents/University/Multimedia Search & Retrieval/project/MSR/resultfiles/similarityScores.txt")
+location <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/MSR/Relevance/Location/location_filter_result.txt", stringsAsFactors = FALSE)
+tfidf <- read.table("/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/MSR/Relevance/TFIDF/similarityScores.txt", stringsAsFactors = FALSE)
+data <- xmlParse("/Volumes/My Passport for Mac/div-2014/devset/devset_topics.xml")
+xml_data <- xmlToList(data)
 
-# get only the half of the file
-tfidf <- tfidf[1:(nrow(tfidf)/2), ]
-
-result <- data.frame()
+result <- NULL
 
 # calculate new similarity score with (tfidf+location)/2
 for(i in 1:nrow(tfidf)) {
+  loc = 0
+  for(j in 1:length(xml_data)) {
+    title <- xml_data[j]$topic$title
+    number <- xml_data[j]$topic$number
+    if(tfidf[i, 1] == title) {
+      loc = number
+      break
+    }
+  }
+  
   id <- tfidf[i, 2]
   sim <- tfidf[i, 3] + location[which(location[, 2] == id), 3]/2
-  result <- rbind(result, c(tfidf[i, 1], id, sim))
+  loc <- as.numeric(loc)
+  result <- rbind(result, data.frame(A=loc, B=id, C=sim))
 }
 
-write.table(result, file="C:/Users/Alina/Documents/University/Multimedia Search & Retrieval/project/MSR/resultfiles/tfidf_location_SimilarityScores.txt", row.names = FALSE, col.names = FALSE)
+index <- which(duplicated(result[, 2]))
+result <- result[-index, ]
+
+write.table(result, file="/Users/Julia/Documents/Universität/MultimediaSearchAndRetrieval/Projekt/MSR/Relevance/Location/preprocessing/tfidf_location_SimilarityScores.txt", row.names = FALSE, col.names = FALSE)
